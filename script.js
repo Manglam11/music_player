6ab106b37f62cad0996ed7edcaa3328ac8914357
MANGLAM DUBEY <manglamdubey11@gmail.com>
2024-05-14 04:16:56 +0530

    sortSongs completed.

--- script.js
+++ script.js
@@ -108,4 +108,19 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
+const sortSongs = () => {
+  userData?.songs.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
+  });
+  return userData?.songs;
+};
+
 renderSongs(userData?.songs);
