ddd5e895563d48e50ed90554a9442ec23545a7c5
MANGLAM DUBEY <manglamdubey11@gmail.com>
2024-05-14 15:46:13 +0530

    highlight currentSong functionality added.

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ const playSong = (id) => {
   userData.currentSong = song;
   playButton.classList.add("playing");
 
+  highlightCurrentSong();
   audio.play();
 };
 
@@ -127,6 +128,19 @@ const playPreviousSong = () => {
 
     playSong(previousSong.id);
   }
+};
+
+const highlightCurrentSong = () => {
+  const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(
+    `song-${userData?.currentSong?.id}`
+  );
+
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
+
+  if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
 };
 
 const renderSongs = (array) => {
