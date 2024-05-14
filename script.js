46e3a76f03d9418758c6f8678d2dd73d3f7adb2a
MANGLAM DUBEY <manglamdubey11@gmail.com>
2024-05-14 12:30:44 +0530

    pause song functionality added.

--- script.js
+++ script.js
@@ -101,6 +101,13 @@ const playSong = (id) => {
   audio.play();
 };
 
+const pauseSong = () => {
+  userData.songCurrentTime = audio.currentTime;
+
+  playButton.classList.remove("playing");
+  audio.pause();
+};
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
@@ -130,6 +137,8 @@ playButton.addEventListener("click", () => {
     playSong(userData?.currentSong.id);
   }
 });
+
+pauseButton.addEventListener("click", pauseSong);
 
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
