904520bd289a0af895805ed2c8b9ba7de96954ee
MANGLAM DUBEY <manglamdubey11@gmail.com>
2024-05-14 12:22:34 +0530

    playbutton functionality is added.

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ const renderSongs = (array) => {
     .map((song) => {
       return `
       <li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
           <span class="playlist-song-title">${song.title}</span>
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
@@ -122,6 +122,14 @@ const renderSongs = (array) => {
 
   playlistSongs.innerHTML = songsHTML;
 };
+
+playButton.addEventListener("click", () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    playSong(userData?.currentSong.id);
+  }
+});
 
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
